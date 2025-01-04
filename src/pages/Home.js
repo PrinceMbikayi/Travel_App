1973238309c313bed82f0a54533497cd140c1761
PrinceMbikayi <pmbikayi01@gmail.com>
2025-01-04 17:12:55 +0200

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import Header from "../components/Header";
+
+const Home = () => {
+
+  const [state] = useState({
+    heading: "We are travel friends",
+    paragraph:
+      "Come and join us we travel the most famous and beautiful places in the world",
+  });
+
+  return (
+    <>
+      <Header heading={state.heading} paragraph={state.paragraph}>
+        <button className="btn-default">Get started</button>
+      </Header>
+    </>
+  );
+};
+
+export default Home;
